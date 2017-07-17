89734d2840c4122b2b0d4591c2fefb02b478f055
jin camou <jincamou@gmail.com>
2017-07-16 19:20:12 -0700

    pass spec for service detail

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -6,12 +6,11 @@ describe "the user sign up path" do
     visit '/'
     click_link 'Sign up!'
     expect(page).to have_content 'Sign up'
-    fill_in 'user[user_name]', :with => 'jin'
-    fill_in 'user[email]', :with => 'jin@jin.com'
-    fill_in 'user[password]', :with => 'jinjin'
-    fill_in 'user[password_confirmation]', :with => 'jinjin'
+    fill_in 'user[user_name]', :with => 'user.user_name'
+    fill_in 'user[email]', :with => 'user.email'
+    fill_in 'user[password]', :with => 'user.password'
+    fill_in 'user[password_confirmation]', :with => 'user.password_confirmation'
     click_button 'Sign Up'
-    save_and_open_page
-    expect(page).to have_content "You've successfully signed up!"
+    expect(page).to have_content "Nom Nom Nosh"
   end
 end
